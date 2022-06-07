54be30f4079ddb6229ee4e28978317c4d6d0d8bf
Theorin F <43284735+TFGrump@users.noreply.github.com>
2022-06-07 12:06:25 -0400

    Add files via upload
    + finished writing random_aisle_generator.py

--- random_aisle_generator.py
+++ random_aisle_generator.py
@@ -10,35 +10,65 @@ def __generate_aisle__(letter, aisle_number):
     sections = []
     for i in range(0, MAX_SECTION):
         sections.append(__generate_section__())
-    return [letter + str(aisle_number), sections]
+    return ['\'' + letter + str(aisle_number) + '\'', sections]
 
 
 def __generate_section__():
     return random.randrange(MIN_MOD, MAX_MOD)
 
 
-def assign_aisles():
+def __get_items_from_commodity__(cursor, commodity):
+    command = 'SELECT product_id FROM product WHERE commodity=\'{}\''.format(commodity)
+    cursor.execute(command)
+    items = []
+    for (product_id,) in cursor:
+        items.append(product_id)
+    return items
+
+
+def __get_commods_from_db__(cursor):
+    order = []
+    command = 'SELECT commodity FROM product GROUP BY commodity ORDER BY 1'
+    cursor.execute(command)
+    for (commodity,) in cursor:
+        order.append(commodity)
+    return order
+
+
+def __get_commodities__(cursor):
+    commodities = __get_commods_from_db__(cursor)
+
+    items_by_commodity = []
+    for commodity in commodities:
+        items_by_commodity.append(__get_items_from_commodity__(cursor, commodity))
+
+    return items_by_commodity
+
+
+def assign_aisles(cnx, cursor):
     columns = ['aisle', 'section', 'modular', 'product_id', 'facing', 'capacity']
-    number_of_items = 3543
-    aisle_number = 1
-    aisle = __generate_aisle__('A', aisle_number)
-    section = 0
-    mods_used = aisle[1][section]
-    aisles = [aisle]
+    items_by_commodity = __get_commodities__(cursor)
 
-    for i in range(0, number_of_items):
-        if i - mods_used == 0:
-            print("MAX_MOD: {}\n".format(aisle[1][section]))
-            section += 1
-            if section >= MAX_SECTION:
-                aisle_number += 1
-                aisle = __generate_aisle__('A', aisle_number)
-                aisles.append(aisle)
-                section = 0
-            mods_used += aisle[1][section]
+    aisle_letter = 'A'
+    for commodity in items_by_commodity:
+        aisle_number = 1
+        aisle = __generate_aisle__(aisle_letter, aisle_number)
+        section = 0
+        mods_used = aisle[1][section]
+        for i in range(0, len(commodity)):
+            if i - mods_used == 0:
+                section += 1
+                if section >= MAX_SECTION:
+                    aisle_number += 1
+                    aisle = __generate_aisle__(aisle_letter, aisle_number)
+                    section = 0
+                mods_used += aisle[1][section]
 
-        mod_used = aisle[1][section] + 1 - (mods_used - i)
-        vals = [aisle[0], section + 1, mod_used, 1, 1, 1]
-        command = indata.insert_data('salesfloor', columns, vals)
-        print("{}".format(command))
-    print(aisles)
+            random_face = int(random.random() * 4 + 1)
+            random_capacity = random_face * (int(random.random() * 4 + 1))
+            mod_used = aisle[1][section] + 1 - (mods_used - i)
+            vals = [aisle[0], section + 1, mod_used, commodity[i], random_face, random_capacity]
+            indata.insert_data(cursor, 'salesfloor', columns, vals)
+        aisle_letter = chr(ord(aisle_letter) + 1)
+
+    cnx.commit()
